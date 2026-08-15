1a986cff8683e2afa805e78a1cae637eb6b6c25f
Ameer <Ameer@LAPTOP-3KITIDVE>
2026-08-15 23:20:18 +0530

    Add project files.

---
+++ EmpManager - Copy (2)/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmpManager.Models;
+
+namespace EmpManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee>? Employees { get; set; }
+    }
+}
---
+++ EmpManager - Copy/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmpManager.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Department { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Salary { get; set; }
+    }
+}
